117f628656abf6d998fad3a5e9795acf570abcef
Daniel Loomb <daniel@statsig.com>
2024-02-08 09:41:35 -0800

    Remove old example

--- example/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../include/statsig_client.h"
-
-int main() {
-  auto client = new statsig::StatsigClient("", nullptr, nullptr);
-
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
-}
