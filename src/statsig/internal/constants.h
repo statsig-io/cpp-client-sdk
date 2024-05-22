8212e71fe0e2ef70a1af3dcdb4ac01b5711b050a
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2024-05-22 10:27:13 -0700

    chore: bump version to 0.0.6

--- src/statsig/internal/constants.h
+++ src/statsig/internal/constants.h
@@ -7,7 +7,7 @@
 namespace statsig::constants {
 
 // Statsig Metadata
-static const char *kSdkVersion = "0.0.5";
+static const char *kSdkVersion = "0.0.6";
 
 // HTTP Endpoints
 static const char *kEndpointInitialize = "/v1/initialize";
